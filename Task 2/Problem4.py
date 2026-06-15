4052ec0623406fa8a5fe95ba72af2e29f23c22cb
rasheda_chowdhury <rashedasanju@gmail.com>
2026-06-15 22:46:10 +0600

    Implement pattern matching function in Problem4.py

--- Task 2/Problem4.py
+++ Task 2/Problem4.py
@@ -1,1 +1,17 @@
+def pattern_matching(pattern, genome):
+    positions = []
+    k = len(pattern)
+
+    for i in range(len(genome) -k +1):
+        if genome[i:i+k] == pattern :
+            positions.append(i)
+
+    return positions
+
+pattern = input()
+genome = input()  
+
+result = pattern_matching(pattern, genome)
+print("".join(map(str, result)))
+
 

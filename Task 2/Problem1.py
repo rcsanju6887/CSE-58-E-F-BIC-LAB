3606f2e520511102d50a12cdc89aa64d9fc7e3c4
rasheda_chowdhury <rashedasanju@gmail.com>
2026-06-15 22:02:11 +0600

    Implement pattern counting function
    Add function to count occurrences of a pattern in text

--- Task 2/Problem1.py
+++ Task 2/Problem1.py
@@ -1,1 +1,13 @@
+def count_pattern(text, pattern):
+    count = 0
+    k = len(pattern)
+    for i in range(len(text)-k +1):
+        if text[i:i+k] == pattern:
+            count +=1
+           
+    return count 
+        
+text = input()
+pattern = input()
+print(count_pattern(text,pattern))
 

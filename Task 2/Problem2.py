75797c863920cd3a441f9accff1679cd4fe2bd55
rasheda_chowdhury <rashedasanju@gmail.com>
2026-06-15 22:20:08 +0600

    Add frequent_words function to find k-mers
    Implement function to find most frequent k-mers in a text.

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,23 @@
+def frequent_words(text, k):
+    count_dict = {}
+
+    for i in range(len(text)-k+1):
+        kmer = text[i:i+k]
+        if kmer in count_dict:
+            count_dict[kmer] += 1
+        else :
+            count_dict[kmer] =1
+
+    max_count = max(count_dict.values())
+
+    result =[]
+    for kmer , count in count_dict.items():
+        if count == max_count:
+            result.append(kmer)
+
+    return result
+text = input()
+k = int(input())
+
+print("".join(frequent_words(text, k)))
 
